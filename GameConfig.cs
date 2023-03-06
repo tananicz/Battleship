622e22d0b76c6a2386b3e11c7bfdd8920b48e0cc
tananicz <tomasz.ananicz@gmail.com>
2023-03-06 10:04:30 +0100

    comment added to ShipSizes array

--- GameConfig.cs
+++ GameConfig.cs
@@ -10,7 +10,7 @@ namespace Battleship
     internal class GameConfig
     {
         public static readonly int BoardSize = 10;
-        public static readonly int[] ShipSizes = { 5, 4, 3, 2, 2, 1, 1 };
+        public static readonly int[] ShipSizes = { 5, 4, 3, 2, 2, 1, 1 };   //num of elements in array equals to num of ships, each integer in an array corresponds to size of a ship
         public static readonly int TotalShipsShots = ShipSizes.Sum();
         public static readonly Point Player1DrawingRectLocation = new Point(10, 40);
         public static readonly Point Player2DrawingRectLocation = new Point(335, 40);
